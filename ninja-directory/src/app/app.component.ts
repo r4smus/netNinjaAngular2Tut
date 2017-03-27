e55d0e5595e694cc7d6150214e23d95cd6fdc771
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-27 13:35:53 +0200

    #14: Custom property binding @Input (get an object from a parent compoent to a child component)

--- ninja-directory/src/app/app.component.ts
+++ ninja-directory/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works! WOOOP, WOOP!';
+  ninja = {
+    name: "Chris",
+    belt: "black"
+  };
 }
--- ninja-directory/src/app/home/home.component.ts
+++ ninja-directory/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   homeTitle: string = "Welcome to the homepage...";
-  ninja = {
-    name:  "Yoshi",
-    belt:  "black"
-  };
+  @Input() ninja;
+
   constructor() { }
 
   ngOnInit() {
