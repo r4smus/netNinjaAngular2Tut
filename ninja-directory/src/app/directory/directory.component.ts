3e8e1fef1282a7b50255a2c063a7504ba56eda97
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-29 19:31:48 +0200

    #22: Custom Filter Pipe

--- ninja-directory/src/app/directory/directory.component.ts
+++ ninja-directory/src/app/directory/directory.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FilterPipe } from '../filter.pipe';
 
 @Component({
   selector: 'app-directory',
