2e2cce4dbaeee28547be3e5f1b4120c639dd6b07
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-30 13:49:27 +0200

    #24: http services

--- ninja-directory/src/app/directory/directory.component.ts
+++ ninja-directory/src/app/directory/directory.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LoggingService } from '../logging.service';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
-  styleUrls: ['./directory.component.css']
+  styleUrls: ['./directory.component.css'],
+  providers: [DataService]
 })
 export class DirectoryComponent implements OnInit {
-  ninjas = [
-    {name: "Yoshi", belt: 'black'},
-    {name: "Ryu", belt: 'red'},
-    {name: "Crystal", belt: 'purple'}
-  ];
+  ninjas = [];
 
-  constructor(private logger: LoggingService) {}
+  constructor(private logger: LoggingService, private dataService: DataService) {}
 
   ngOnInit() {
+    this.dataService.fetchData().subscribe(
+     (data) => this.ninjas = data 
+    );
   }
 
   logIt(){
     this.logger.log();
   }
+
 
 }
