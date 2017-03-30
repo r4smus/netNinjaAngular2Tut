28cf59c03520694d546fd9379c3c8e7f8818b647
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-30 12:54:55 +0200

    #22: dont need that import

--- ninja-directory/src/app/directory/directory.component.ts
+++ ninja-directory/src/app/directory/directory.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FilterPipe } from '../filter.pipe';
 
 @Component({
   selector: 'app-directory',
