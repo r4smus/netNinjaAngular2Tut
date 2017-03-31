c4e056ce3e015925c082bacf84566d9c61b67398
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-31 11:50:44 +0200

    #26: Firebase api

--- ninja-directory/src/app/directory/directory.component.ts
+++ ninja-directory/src/app/directory/directory.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { LoggingService } from '../logging.service';
 import { DataService } from '../data.service';
 
+declare var firebase: any;
+
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
@@ -15,13 +17,21 @@ export class DirectoryComponent implements OnInit {
   constructor(private logger: LoggingService, private dataService: DataService) {}
 
   ngOnInit() {
-    this.dataService.fetchData().subscribe(
+    /*this.dataService.fetchData().subscribe(
      (data) => this.ninjas = data 
-    );
+    );*/
+
+    this.fbGetData();
   }
 
   logIt(){
     this.logger.log();
+  }
+
+  fbGetData(){
+    firebase.database().ref('/').on('child_added', (snapshot) => {
+      this.ninjas.push(snapshot.val())
+    })
   }
 
 
